8969e96614dcf2a22575175b7761c07275b4b494
lansanacisse <114141078+lansanacisse@users.noreply.github.com>
2023-12-26 16:05:03 +0100

    continuer_partie.test.js

--- continuer_partie.test.js
+++ continuer_partie.test.js
@@ -1,26 +1,37 @@
-import { chargerFichierJson, fichierJsonExiste, chargerDonnees, afficherPageDifficultes, afficherAlerte } from './continuer_partie';
+const fs = require('fs');
+const { chargerFichierJson, fichierJsonExiste, chargerDonnees, afficherPageDifficultes, afficherAlerte } = require('./continuer_partie');
 
 // Mock pour fs.existsSync et autres interactions avec le système de fichiers
-const fs = require('fs');
 jest.mock('fs');
+
+// Mock des fonctions internes si elles interagissent avec des systèmes externes
+jest.mock('./continuer_partie', () => ({
+    chargerDonnees: jest.fn(),
+    afficherPageDifficultes: jest.fn(),
+    afficherAlerte: jest.fn(),
+    // Assurez-vous d'inclure les autres fonctions exportées ici si nécessaire
+}));
 
 describe('Tests pour continuer_partie.js', () => {
     beforeEach(() => {
         // Réinitialiser les mocks avant chaque test
         fs.existsSync.mockClear();
+        chargerDonnees.mockClear();
+        afficherPageDifficultes.mockClear();
+        afficherAlerte.mockClear();
     });
 
     test('chargerFichierJson avec fichier JSON existant', () => {
-        // Simuler l'existence d'un fichier JSON
         fs.existsSync.mockReturnValue(true);
 
-        // Exécutez chargerFichierJson et vérifiez le comportement attendu
         chargerFichierJson();
-        // Ajoutez des assertions pour vérifier si les fonctions attendues ont été appelées
+
+        // Vérifiez que chargerDonnees et afficherPageDifficultes ont été appelées
+        expect(chargerDonnees).toHaveBeenCalled();
+        expect(afficherPageDifficultes).toHaveBeenCalled();
     });
 
     test('chargerFichierJson gère les exceptions', () => {
-        // Simuler une situation qui pourrait provoquer une exception
         fs.existsSync.mockImplementation(() => {
             throw new Error('Erreur système de fichiers');
         });
@@ -29,5 +40,4 @@ describe('Tests pour continuer_partie.js', () => {
             chargerFichierJson();
         }).toThrow('Erreur système de fichiers');
     });
-
 });
